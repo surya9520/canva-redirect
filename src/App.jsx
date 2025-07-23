4f3ffb10c1dc1974485519a83b98582958ca8010
Surya <suryajoshi617@gmail.com>
2025-07-23 09:46:31 +0530

    authcallback

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CanvaRedirect from './pages/CanvaRedirect'
 import ReturnNav from './pages/ReturnNav'
+import AuthCallback from './pages/AuthCallback'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/canvaDesign/:editUrl" element={<CanvaRedirect />} />
       <Route path="/navReturn" element={<ReturnNav/>} />
+      <Route path="/authcallback" element={<AuthCallback />} />
 
     </Routes>
     </BrowserRouter>
---
+++ src/pages/AuthCallback.jsx
@@ -0,0 +1,35 @@
+import React, { use } from 'react'
+import { useLocation } from 'react-router-dom';
+
+const AuthCallback = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const code = searchParams.get('code');
+  const state = searchParams.get('state');
+  const error = searchParams.get('error');
+  const errorDescription = searchParams.get('error_description') ||'something went wrong';
+
+  useEffect(() => {
+    if (code && state) {
+      if (window.opener) {
+          window.opener.postMessage({code, state}, 'https://app-dev-main.socialpilot.co');
+          window.close();
+        } else if (error) {
+        // Send the error info to the opener window if OAuth failed
+        window.opener.postMessage(
+          { error, error_description: errorDescription },
+          'https://app-dev-main.socialpilot.co'
+        );
+        window.close();
+      } else {
+          console.warn('No opener window to post message to.');
+        }
+    }
+  }, []);
+
+  return (
+    <div>AuthCallback</div>
+  )
+}
+
+export default AuthCallback
