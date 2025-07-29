516ff69cf321aeb6a68e684f354e06e4ef097fb3
Surya <suryajoshi617@gmail.com>
2025-07-29 13:00:05 +0530

    here i am

--- src/pages/ReturnNav.jsx
+++ src/pages/ReturnNav.jsx
@@ -16,7 +16,7 @@ useEffect(() => {
         let {correlation_state,design_id:designId}=decoded || {};
           const{domain,type,tab,category}= jwtDecode(correlation_state) ||{};
           window.opener.postMessage({designId, type,tab,category}, `${domain}`);
-          // window.close();
+          window.close();
         } else {
           alert('No opener window to post message to.');
           window.close();
